8e2880849bc748d34f6549b2e34a972f93be282b
prokofitch <caiocesar.siqueira21@gmail.com>
2025-04-08 15:05:24 -0300

    Update CORS

--- src/main/java/com/fixable/fixable/config/CorsConfig.java
+++ src/main/java/com/fixable/fixable/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOriginPatterns("http://localhost:3000", "http://localhost:9090", "http://*.fixable.com")
                         .allowedMethods("*")
                         .allowCredentials(true);
             }
